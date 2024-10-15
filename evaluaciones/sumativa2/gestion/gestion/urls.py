2ac12e1b1c884b39d862b8874404e300bb08bf6d
Ignacio <ignaciocontreras911@gmail.com>
2024-10-15 14:20:09 -0300

    up: adjuntado modelo de datos y creacion de templates

--- evaluaciones/sumativa2/gestion/gestion/urls.py
+++ evaluaciones/sumativa2/gestion/gestion/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for gestion project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
+from GPSA import views
 
 urlpatterns = [
+  path('', lambda request: redirect('lista_productos/')),
     path('admin/', admin.site.urls),
+    path('crear/', views.crear_producto, name='lista_productos'),
+    path('editar/<int:id>/', views.editar_producto, name='editar_producto'),
+    path('eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
 ]
